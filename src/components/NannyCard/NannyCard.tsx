da8897d76ef2aa1fe6f1e27dde52d79adfa79294
Mariana_k <m.koval000@gmail.com>
2026-01-02 13:38:51 +0200

    add NannyCard

--- src/components/NannyCard/NannyCard.tsx
+++ src/components/NannyCard/NannyCard.tsx
@@ -24,14 +24,69 @@ export default function NannyCard() {
           <svg className={css.block_icon} width={16} height={16}>
             <use href="/sprite.svg#icon-Location"></use>
           </svg>
-          {/* <p className={css.address_text}>{nanny.location}</p> */}
+          {/* <p className={css.block_text}>{nanny.location}</p> */}
         </div>
         <span className={css.box_block_span}>|</span>
         <div className={css.rating}>
           <svg className={css.block_icon} width={16} height={16}>
             <use href="/sprite.svg#icon-star"></use>
           </svg>
+          {/* <p className={css.block_text}>Rating:{nanny.rating}</p> */}
         </div>
+        <span className={css.box_block_span}>|</span>
+        <div className={css.price}>
+          <p className={css.block_text}>
+            Price / 1 hour:
+            {/* <span className={css.span_green}>{nanny.price_per_hour}$</span> */}
+          </p>
+        </div>
+      </div>
+      <div className={css.age}>
+        <p className={css.private_text}>
+          {/* Age: <span className={css.span_age}>{nanny.age}</span> */}
+        </p>
+      </div>
+      <div className={css.experience}>
+        <p className={css.private_text}>
+          {/* Experience: <span className={css.span_age}>{nanny.experience}</span> */}
+        </p>
+      </div>
+      <div className={css.kids_age}>
+        <p className={css.private_text}>
+          {/* Kids Age: <span className={css.span_age}>{nanny.kids_age}</span> */}
+        </p>
+      </div>
+      <div className={css.characters}>
+        <p className={css.private_text}>
+          {/* Characters: <span className={css.span_age}>{nanny.characters}</span> */}
+        </p>
+      </div>
+      <div className={css.education}>
+        <p className={css.private_text}>
+          {/* Education: <span className={css.span_age}>{nanny.education}</span> */}
+        </p>
+      </div>
+      {/* <p className={css.about}>{nanny.about}</p> */}
+      <div className={css.review}>
+        <div className={css.review_info}>
+          <div className={css.review_name}>
+            <div className={css.name_one_letter}>
+              {/* <p className={css.name_one_text}>{nanny.reviews.reviewer.slice(0, 1)}</p> */}
+            </div>
+            <div className={css.name_full}>
+              {/* <p className={css.name_full_black}>{nanny.reviews.reviewer.slice(0, 5)}.</p> */}
+              <svg className={css.name_full_icon} width={16} height={16}>
+                <use href="/sprite.svg#icon-star"></use>
+              </svg>
+              {/* <p className={css.name_full_text}>{nanny.reviews.rating}</p> */}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={css.btn}>
+        <button type="button" className={css.card_btn}>
+          Make an appointment
+        </button>
       </div>
     </div>
   );
